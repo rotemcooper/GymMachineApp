9832fb14135c9d9d0e06e7c0d7a7abc66be48582
roc <rotemcooper@gmail.com>
2018-12-30 18:51:54 -0800

    Changes

--- GymMachineApp/src/main/java/com/felhr/serialportexample/WorkoutSelector.java
+++ GymMachineApp/src/main/java/com/felhr/serialportexample/WorkoutSelector.java
@@ -2,12 +2,29 @@ package com.felhr.serialportexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class WorkoutSelector extends AppCompatActivity {
+
+    private Spinner activityTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_selector);
+
+        activityTypeSpinner = (Spinner) findViewById(R.id.activityTypeSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activityTypes, R.layout.workout_selector_spinner);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.workout_selector_spinner);//android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        activityTypeSpinner.setAdapter(adapter);
     }
+
+
 }
