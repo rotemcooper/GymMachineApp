a096bb53e0f1f7185fe7e66d85c718ed14995b3b
roc <rotemcooper@gmail.com>
2019-01-03 22:16:08 -0800

    Changes

--- GymMachineApp/src/main/java/com/felhr/serialportexample/WorkoutSelector.java
+++ GymMachineApp/src/main/java/com/felhr/serialportexample/WorkoutSelector.java
@@ -15,28 +15,30 @@ public class WorkoutSelector extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_selector);
 
-        activityTypeSpinner = (Spinner) findViewById(R.id.activityTypeSpinner);
+        //-----------------------------------------------------------------------
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        // Load activity type data with ArrayAdapter using the string array and a spinner layout
         ArrayAdapter<CharSequence> activityTypeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.activityTypes, R.layout.workout_selector_spinner);
 
         // Specify the layout to use when the list of choices appears
         activityTypeAdapter.setDropDownViewResource(R.layout.workout_selector_spinner_dropdown);
+
         // Apply the adapter to the spinner
+        activityTypeSpinner = (Spinner) findViewById(R.id.activityTypeSpinner);
         activityTypeSpinner.setAdapter(activityTypeAdapter);
 
-        //------------------------------
+        //-----------------------------------------------------------------------
 
-        workoutAreaSpinner = (Spinner) findViewById(R.id.workoutAreaSpinner);
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        // Load workout data with ArrayAdapter using the string array and a spinner layout
         ArrayAdapter<CharSequence> workoutAreaAdapter = ArrayAdapter.createFromResource(this,
                 R.array.workoutAreas, R.layout.workout_selector_spinner);
 
         // Specify the layout to use when the list of choices appears
         workoutAreaAdapter.setDropDownViewResource(R.layout.workout_selector_spinner_dropdown);
+
         // Apply the adapter to the spinner
+        workoutAreaSpinner = (Spinner) findViewById(R.id.workoutAreaSpinner);
         workoutAreaSpinner.setAdapter(workoutAreaAdapter);
 
     }
